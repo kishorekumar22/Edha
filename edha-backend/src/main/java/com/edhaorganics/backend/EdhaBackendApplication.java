72ca9c8ef1ea89b6f29ed4a2799c78e707c605cf
kishorekumar.s <kishorekumar.s@appviewxcom>
2018-11-25 21:48:57 +0530

    Backend : Payment changes done

--- edha-backend/src/main/java/com/edhaorganics/backend/EdhaBackendApplication.java
+++ edha-backend/src/main/java/com/edhaorganics/backend/EdhaBackendApplication.java
@@ -1,5 +1,9 @@
 package com.edhaorganics.backend;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -7,6 +11,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 public class EdhaBackendApplication {
+	@PostConstruct
+	  public void init(){
+	    // Setting Spring Boot SetTimeZone
+	    TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdhaBackendApplication.class, args);
